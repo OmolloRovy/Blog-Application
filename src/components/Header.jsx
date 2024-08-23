795814c830e1af12be6204f8840a377b74fdb525
omolloRovy <masimevanistelrooy@gmail.com>
2024-08-23 15:54:39 +0300

    LOGO

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+import  Logo from '../assets/Logo.jpeg'
 const Header = () => {
   return (
    <nav>
     <div className="container nav__container">
-      <Link to="/" className='nav__logo'></Link>
+      <Link to="/" className='nav__logo'>
+      <img src="Logo" alt=""/>
+      </Link>
     </div>
 
    </nav>
