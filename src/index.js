0ca7812e01f4246e5e2906562945162a128fa944
omolloRovy <masimevanistelrooy@gmail.com>
2024-08-23 12:46:23 +0300

    routers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage'
 
+const router = createBrowserRouter([
+{
+  path:"/",
+  element: <Layout />,
+  errorElement: <ErrorPage.jsx />
+}
+
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <h1>BLOG</h1>
+  <RouterProvider router={router} />
   </React.StrictMode>
 );
