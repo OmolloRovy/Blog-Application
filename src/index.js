d5087c500eac33de68f545f2b01831de7fa1817c
omolloRovy <masimevanistelrooy@gmail.com>
2024-08-23 14:35:30 +0300

    create posts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
 import Layout from './components/Layout';
 import ErrorPage from './pages/ErrorPage'
 import Home from './pages/Home'
 import PostDetail from './pages/PostDetails'
 import Register from './pages/Register'
-
+import Login from './pages/Login'
+import UserProfile from './pages/UserProfile'
+import Authors from './pages/Authors'
+import EditPosts from './pages/EditPosts'
+import AuthorsPosts from './pages/AuthorsPosts'
+import Logout from './pages/Logout'
+import createPosts from './pages/createPosts'
 
 const router = createBrowserRouter([
 {
@@ -17,7 +24,16 @@ const router = createBrowserRouter([
   children: [
    {index: true, element: <Home />},
    {path:"posts/:id", element: <PostDetail />},
-   {register:"register", element: <Register />}
+   {path:"register", element: <Register />},
+   {path:"login", element: <Login />},
+   {path:"profile:/id", element: <UserProfile />},
+   {path:"authors", element: <Authors />},
+   {path:"create", element: <createPosts />},
+   {path:"posts:id/edit", element: <EditPosts />},
+   {path:"posts/categories:category", element: <categoryPosts />},
+   {path:"posts/users:id", element: <AuthorsPosts />},
+   {path:"posts:id/edit", element: <EditPosts />},
+   {path:"logout", element: <Logout />},
   ]
 }
 
