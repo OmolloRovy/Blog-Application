2af343c4ab9b38f8db9504547e1017a25daba747
omolloRovy <masimevanistelrooy@gmail.com>
2024-08-23 16:44:55 +0300

    header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import  Logo from '../assets/Logo.jpeg'
+import { FaBars } from "react-icons/fa";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
 const Header = () => {
   return (
    <nav>
     <div className="container nav__container">
       <Link to="/" className='nav__logo'>
-      <img src="Logo" alt=""/>
+      <img src={Logo} alt=""/>
       </Link>
+<ul className="nav__menu">
+  
+    <li><Link to="/profile">Earnest Achiever</Link>  </li>
+    <li><Link to="/create" >Create Post </Link>  </li>
+    <li><Link to="/authors">Authors</Link>  </li>
+    <li><Link to="/logout">Logout</Link>  </li>
+   </ul>
+   <button className="nav__toggle-btn">
+<AiOutlineCloseCircle />
+   </button>
     </div>
 
    </nav>
