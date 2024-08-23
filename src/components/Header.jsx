2b6f35cdc3f110a17578a7950e51cc3e90abae50
omolloRovy <masimevanistelrooy@gmail.com>
2024-08-23 15:40:23 +0300

    assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      Header
+   <nav>
+    <div className="container nav__container">
+      <Link to="/" className='nav__logo'></Link>
     </div>
+
+   </nav>
   )
 }
 
